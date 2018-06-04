89eeffed189cf870d95ebc66d313f6f836efc1c2
Michele Ferri <>
2018-06-04 22:42:59 +0200

    chrono

--- chrono.js
+++ chrono.js
@@ -1,26 +1,28 @@
 var five = require("johnny-five"),
   board, photoresistor, piezo, led,
+  timerState,
   timeStart, 
   timeCurr1, timeLast1, t1, v1;
 
 const sensorThreshold = 160;
 const ignoreTimeMillis = 1000;
+const STATE_STOPPED = 0;
+const STATE_RUNNING = 1;
 
 board = new five.Board();
 
 board.on("ready", function() {
 
+  timerState = STATE_STOPPED;
   timeStart = Date.now();
   timeCurr1 = timeStart - ignoreTimeMillis;
 
-  // Create a new `photoresistor` hardware instance.
+  // Create hardware instances.
   photoresistor = new five.Sensor({
     pin: "A0",
     freq: 5
   });
-
   piezo = new five.Piezo(3);
-
   led = new five.Led(13);
 
   // Inject the `sensor` hardware into
@@ -32,31 +34,63 @@ board.on("ready", function() {
     led: led
   });
 
-  led.on();
+  process.stdin.on("keypress", function(_, key) {
+		if (!key) {
+			return;
+    }
+    
+    console.log(key.name);
 
-  // "data" get the current reading from the photoresistor
-  photoresistor.on("data", function() {
-    t1 = Date.now();
-    v1 = this.value;
-    console.log(v1);
-    if (v1 >= sensorThreshold && t1 >= timeCurr1+ignoreTimeMillis) {
-      console.log(t1-timeStart);
-      timeCurr1 = t1;
-      piezo.frequency(750, 50);
+		switch (key.name) {
+      case "l":
+        led.on();
+        break;
+      case 's':
+        startTimer();
+        break;
+      case 'p':
+        playSong();
+        break;
     }
   });
 
-  // piezo.play({
-  //   // song is composed by a string of notes
-  //   // a default beat is set, and the default octave is used
-  //   // any invalid note is read as "no note"
-  //   song: "C D F D A - A A A A G G G G - - C D F D G - G G G G F F F F - -",
-  //   beats: 1 / 4,
-  //   tempo: 100
-  // });
+  // "data" get the current reading from the photoresistor
+  photoresistor.on("data", function() {
+    if (timerState != STATE_RUNNING) {
+      return;
+    }
+    handleInput(this.value);
+  });
 });
 
 board.on("exit", function() {
   led.off();
   piezo.noTone();
 });
+
+var startTimer = function() {
+  timerState = STATE_RUNNING;
+  playSong();
+};
+
+var playSong = function() {
+  piezo.noTone();
+  piezo.play({
+    // song is composed by a string of notes
+    // a default beat is set, and the default octave is used
+    // any invalid note is read as "no note"
+    song: "C D F D A - A A A A G G G G - - C D F D G - G G G G F F F F - -",
+    beats: 1 / 4,
+    tempo: 100
+  });
+};
+
+var handleInput = function(data) {
+  t1 = Date.now();
+  // console.log(v1);
+  if (data >= sensorThreshold && t1 >= timeCurr1+ignoreTimeMillis) {
+    console.log(t1-timeStart);
+    timeCurr1 = t1;
+    piezo.frequency(750, 50);
+  }
+};
