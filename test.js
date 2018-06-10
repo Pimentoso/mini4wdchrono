3e5d1366e2733517bd8ffd01dd083f7e858473ec
Michele Ferri <>
2018-06-10 13:47:34 +0200

    test.js with real proximity sensor

---
+++ test.js
@@ -0,0 +1,56 @@
+var temporal = require("temporal");
+var five = require("johnny-five");
+var board, sensor, piezo, led;
+
+const sensorThreshold = 160;
+
+board = new five.Board();
+
+board.on("ready", function() {
+
+  // Create hardware instances.
+  sensor = new five.Sensor.Digital(5);
+  piezo = new five.Piezo(3);
+  led = new five.Led(13);
+
+  board.repl.inject({
+    s1: sensor,
+    p1: piezo,
+    l1: led,
+  });
+
+  sensor.on("change", function() {
+    handleInput(this.value);
+  });
+});
+
+board.on("exit", function() {
+  led.off();
+  piezo.noTone();
+});
+
+var playSound = function() {
+  piezo.noTone();
+  piezo.frequency(1000, 100);
+};
+
+var blinkLed = () => {
+  led.on();
+  temporal.queue([{
+    wait: 200,
+    task: () => {
+      led.off();
+    }
+  }]);
+};
+
+var handleInput = function(data) {
+  if (data == 0) {
+    led.on();
+    // playSound();
+  }
+  else {
+    led.off();
+    // piezo.noTone();
+  }
+};
