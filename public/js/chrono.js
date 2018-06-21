3552927ce752eb425453a1920479eacb10a71d54
Michele Ferri <>
2018-06-21 23:28:13 +0200

    wip

--- public/js/chrono.js
+++ public/js/chrono.js
@@ -1,10 +1,9 @@
 'use strict';
 
 var rCar0 = {}, rCar1 = {}, rCar2 = {}, rCars = []; 
-var rLaneOrder = [0,1,2];
-
-var rTrackLength = 150; // TODO
-var rTimeThreshold = 0.4; // TODO
+var rLaneOrder = [0,1,2]; // TODO from interface
+var rTrackLength = 150; // TODO from interface
+var rTimeThreshold = 0.4; // TODO from interface
 var rSpeedThreshold = 6; // speed in m/s to calculate cutoff
 var rTimeCutoffMin = 0; // min lap cutoff
 var rTimeCutoffMax = 0; // max lap cutoff
@@ -48,16 +47,18 @@ const chronoInit = (playerIds) => {
     rCars = [rCar0, rCar1, rCar2];
 };
 
+// TODO ping function to stop race if no one finishes
+
 const chronoAddLap = (currLane) => {
     var rTempTime = new Date().getTime();
-    var rTempCars = _.filter(rCars, function(c){ return currLane == c.nextLane; });
+    var rTempCars = _.filter(rCars, (c) => { currLane == c.nextLane });
     var rTempCar;
     if (_.size(rTempCars) == 1) {
         rTempCar = rTempCars[0];
     }
     else {
-        // TODO FILTER BY THRESHOLD
-
+        markCarsOutOfBounds(rTempCars, rTempTime);
+        rTempCar = filterCarsByThreshold(rTempCars, rTempTime);
     }
 
     // handle the car
@@ -79,9 +80,15 @@ const nextLane = (currLane) => {
 };
 
 const filterCarsByThreshold = (cars, currTime) => {
-    // TODO filter min threshold
-    cars = _.filter(cars, function(c){ return true; });
-    // TODO filter max threshold and set outOfBounds and totalTime = 99999
-    cars = _.filter(cars, function(c){ return true; });
+    return _.find(cars, (c) => { 
+        (currTime - c.currTime > rTimeCutoffMax) && (currTime - c.currTime < rTimeCutoffMin);
+    });
+};
+
+const markCarsOutOfBounds = (cars, currTime) => {
+    _.each(_.filter(cars, (c) => { currTime - c.currTime > rTimeCutoffMax; }), (c) => {
+        c.totalTime = 99999;
+        c.outOfBounds = true;
+    });
 };
 
