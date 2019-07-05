013df87428931f22a4ab467b5a9607070b5bb9cb
Pimentoso <zomgsako@gmail.com>
2019-07-05 21:35:23 +0200

    fix postinstall script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,6 +1,7 @@
 const { exec } = require('child_process');
+const path = require('path')
 
-exec("./node_modules/.bin/electron -v", (_1, stdout, _2) => {
+exec(path.join('node_modules', '.bin', 'electron -v'), (_1, stdout, _2) => {
     var electron_version = stdout.trim();
     exec("./node_modules/.bin/electron-rebuild -v " + electron_version);
 });
