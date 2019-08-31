c87c527cc9b147d154b8eee1ce9e3a58d6f92df5
Pimentoso <zomgsako@gmail.com>
2019-08-31 10:54:27 +0200

    win64 build script

--- utils/build-win64.ps1
+++ utils/build-win64.ps1
@@ -1,10 +1,11 @@
 cd $PSScriptRoot
 cd ..
-echo "Deleting release-builds directory"
-Remove-Item –path release-builds/ –recurse
+Write-Host "Deleting release-builds directory"
+Remove-Item -path .\release-builds\ -recurse
 electron-packager . Mini4wdChrono --overwrite --asar --icon=images/ic_launcher_web.ico --prune=true --out=release-builds
 cd release-builds
 $relname = Get-ChildItem -Path . -Force -Recurse | Select-Object -First 1
-echo "Creating $relname.zip archive"
-Compress-Archive -Path $relname/ -DestinationPath $relname.zip
-echo "Completed"
+Write-Host "Creating $relname.zip archive"
+Compress-Archive -Path "$relname/" -DestinationPath "$relname.zip"
+cd ..
+Write-Host "Completed"
