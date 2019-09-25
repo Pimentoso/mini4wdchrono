e08a671d198d3bec0bc96d4e873e33247be5296e
Michele Ferri <zomgsako@gmail.com>
2019-09-25 17:27:40 +0200

    change release zip archive names

--- utils/build-darwin.sh
+++ utils/build-darwin.sh
@@ -6,6 +6,6 @@ rm -rf release-builds/
 $(electron-packager . Mini4wdChrono --overwrite --icon=images/ic_launcher_web.icns --prune=true --out=release-builds)
 cd release-builds
 relname=$(ls | sort -n | head -1)
-echo "Creating $relname.zip archive"
-$(ditto -c -k --sequesterRsrc --keepParent $relname/ $relname.zip)
-echo "Completed"
+echo "Creating zip archive"
+$(ditto -c -k --sequesterRsrc --keepParent $relname/ Mini4wdChrono-mac.zip)
+echo "Done"
--- utils/build-win64.ps1
+++ utils/build-win64.ps1
@@ -5,7 +5,7 @@ Remove-Item -path .\release-builds\ -recurse
 electron-packager . Mini4wdChrono --overwrite --asar --icon=images/ic_launcher_web.ico --prune=true --out=release-builds
 cd release-builds
 $relname = Get-ChildItem -Path . -Force -Recurse | Select-Object -First 1
-Write-Host "Creating $relname.zip archive"
-Compress-Archive -Path "$relname/" -DestinationPath "$relname.zip"
+Write-Host "Creating zip archive"
+Compress-Archive -Path "$relname/" -DestinationPath "Mini4wdChrono-windows.zip"
 cd ..
-Write-Host "Completed"
+Write-Host "Done"
