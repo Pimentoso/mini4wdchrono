5c6095b5ac462eae9a75d81c5e48f74746fe6895
Michele Ferri <zomgsako@gmail.com>
2019-10-06 21:37:29 +0200

    fix #34 - right click on forms and selected text

--- window.js
+++ window.js
@@ -7,9 +7,9 @@ const path = require('path');
 const url = require('url');
 
 if (process.argv[2] == "--watch") {
-  require('electron-reload')(__dirname, {
-    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-  })
+	require('electron-reload')(__dirname, {
+		electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+	})
 }
 
 // Module to control application life.
@@ -18,41 +18,71 @@ const app = electron.app;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
+// Context menu accessor.
+const Menu = electron.Menu;
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 1280, 
-    height: 800,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
+	// Create the browser window.
+	mainWindow = new BrowserWindow({
+		width: 1280,
+		height: 800,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+	// and load the index.html of the app.
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
 
-  // Maximize.
-  mainWindow.maximize();
-  mainWindow.setResizable(false);
+	// Maximize.
+	mainWindow.maximize();
+	mainWindow.setResizable(false);
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+	// Open the DevTools.
+	// mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  });
+	// Emitted when the window is closed.
+	mainWindow.on('closed', function () {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null
+	});
+
+	const selectionMenu = Menu.buildFromTemplate([
+		{ role: 'copy' },
+		{ type: 'separator' },
+		{ role: 'selectall' },
+	])
+
+	const inputMenu = Menu.buildFromTemplate([
+		{ role: 'undo' },
+		{ role: 'redo' },
+		{ type: 'separator' },
+		{ role: 'cut' },
+		{ role: 'copy' },
+		{ role: 'paste' },
+		{ type: 'separator' },
+		{ role: 'selectall' },
+	])
+
+	// Show context menus on right click.
+	mainWindow.webContents.on('context-menu', (e, props) => {
+		const { selectionText, isEditable } = props;
+		if (isEditable) {
+			inputMenu.popup(mainWindow);
+		} else if (selectionText && selectionText.trim() !== '') {
+			selectionMenu.popup(mainWindow);
+		}
+	})
 }
 
 // Prevent multiple instances of this app to run.
@@ -64,18 +94,18 @@ app.makeSingleInstance;
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on('window-all-closed', function () {
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
-app.on('activate', function() {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+app.on('activate', function () {
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow();
+	}
 });
