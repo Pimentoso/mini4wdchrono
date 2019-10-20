7648443a863dc06e656878159bb664e05c02bb8e
Michele Ferri <zomgsako@gmail.com>
2019-10-20 12:07:39 +0200

    strip led manager

--- js/led_manager.js
+++ js/led_manager.js
@@ -12,7 +12,7 @@ class LedManager {
 
 	connected() {
 		this.board.pinMode(this.pinBuzzer, j5.Pin.OUTPUT);
-		this.beep(250);
+		this.beep(100);
 	}
 
 	disconnected() {
@@ -46,13 +46,14 @@ class LedManagerLilypad extends LedManager {
 		this.led2 = new j5.Led(pinLeds[1]);
 		this.led3 = new j5.Led(pinLeds[2]);
 		this.leds = [this.led1, this.led2, this.led3];
+		this.ready = false;
 	}
 
 	connected() {
 		super.connected();
 		// blink all leds for 3 sec
 		this.led1.blink(125); this.led2.blink(125); this.led3.blink(125);
-		utils.delay(() => { this.led1.stop().off(); this.led2.stop().off(); this.led3.stop().off(); }, 3000);
+		utils.delay(() => { this.led1.stop().off(); this.led2.stop().off(); this.led3.stop().off(); this.ready = true; }, 3000);
 	}
 
 	disconnected() {
@@ -85,9 +86,11 @@ class LedManagerLilypad extends LedManager {
 
 	lap(lane) {
 		// flash lane led for 1 sec
-		let led = this.leds[lane];
-		led.on();
-		utils.delay(() => { led.off(); }, 1000);
+		if (this.ready) {
+			let led = this.leds[lane];
+			led.on();
+			utils.delay(() => { led.off(); }, 1000);
+		}
 	}
 }
 
@@ -96,6 +99,7 @@ class LedManagerRgbStrip extends LedManager {
 	constructor(board, pin, pinBuzzer) {
 		super(board, pinBuzzer);
 		this.pin = pin;
+		this.ready = false;
 	}
 
 	connected() {
@@ -106,19 +110,21 @@ class LedManagerRgbStrip extends LedManager {
 			strips: [{ pin: this.pin, length: 9 },],
 			gamma: 2.8
 		});
-		var stripp = this.strip;
+		var manager = this;
 
 		this.strip.on("ready", function () {
-			stripp.pixel(0).color('#188bc8');
-			stripp.pixel(3).color('#e62227');
-			stripp.pixel(6).color('#f8f8f8');
-			stripp.show();
+			manager.strip.pixel(0).color('#188bc8');
+			manager.strip.pixel(3).color('#e62227');
+			manager.strip.pixel(6).color('#f8f8f8');
+			manager.strip.show();
 
-			setInterval(function () {
-				stripp.shift(1, pixel.FORWARD, true);
-				stripp.show();
+			let shift = setInterval(function () {
+				manager.strip.shift(1, pixel.FORWARD, true);
+				manager.strip.show();
 			}, 125);
-			utils.delay(() => { stripp.off(); }, 3000);
+			utils
+				.delay(() => { clearInterval(shift); }, 3000)
+				.delay(() => { manager.strip.off(); manager.ready = true; }, 125);
 		});
 	}
 
@@ -128,7 +134,21 @@ class LedManagerRgbStrip extends LedManager {
 	}
 
 	roundStart(startTimerCallback) {
-
+		var stripp = this.strip;
+		this.beep(1500);
+		utils
+			.delay(() => { stripp.off(); }, 1500)
+			.delay(() => { stripp.pixel(0).color('#ff0100'); stripp.show(); }, 400)
+			.delay(() => { stripp.pixel(1).color('#ff0100'); stripp.show(); }, 400)
+			.delay(() => { stripp.pixel(2).color('#ff0100'); stripp.show(); }, 400)
+			.delay(() => { stripp.pixel(3).color('#ff0100'); stripp.show(); }, 400)
+			.delay(() => { stripp.pixel(4).color('#ff0100'); stripp.show(); }, 400)
+			.delay(() => { stripp.pixel(5).color('#ff0100'); stripp.show(); }, 400)
+			.delay(() => { stripp.pixel(6).color('#ff0100'); stripp.show(); }, 400)
+			.delay(() => { stripp.pixel(7).color('#ff0100'); stripp.show(); }, 400)
+			.delay(() => { stripp.pixel(8).color('#ff0100'); stripp.show(); }, 400)
+			.delay(() => { stripp.color('#06a14e'); stripp.show(); this.beep(1000); startTimerCallback(); }, 1500)
+			.delay(() => { stripp.off(); }, 2500)
 	}
 
 	roundFinish(cars) {
@@ -136,7 +156,13 @@ class LedManagerRgbStrip extends LedManager {
 		let finishCars = _.filter(cars, (c) => { return !c.outOfBounds && c.lapCount == 4 });
 		_.each(finishCars, (c) => {
 			if (c.position == 1) {
-				colorLane(c.startLane);
+				colorLane(c.startLane, '#ff0100');
+			}
+			else if (c.position == 2) {
+				colorLane(c.startLane, '#ff7400');
+			}
+			else if (c.position == 3) {
+				colorLane(c.startLane, '#fec101');
 			}
 		});
 		this.strip.show();
@@ -144,23 +170,28 @@ class LedManagerRgbStrip extends LedManager {
 
 	lap(lane) {
 		// flash lane led for 1 sec
-		this.colorLane(lane, '#06a14e');
-		this.strip.show();
-		utils.delay(() => {
-			this.clearLane(lane);
-		}, 1000);
+		if (this.ready) {
+			this.colorLane(lane, '#06a14e');
+			utils.delay(() => {
+				this.clearLane(lane);
+			}, 1000);
+		}
 	}
 
 	colorLane(lane, color) {
-		for (let i = lane * 3; i <= 2; ++i) {
+		let start = lane * 3;
+		for (let i = start; i <= start+2; i++) {
 			this.strip.pixel(i).color(color);
 		}
+		this.strip.show();
 	}
 
 	clearLane(lane) {
-		for (let i = lane * 3; i <= 2; ++i) {
+		let start = lane * 3;
+		for (let i = start; i <= start+2; ++i) {
 			this.strip.pixel(i).off();
 		}
+		this.strip.show();
 	}
 }
 
