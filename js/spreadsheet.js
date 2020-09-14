e407df60a9fdaba1c39fd9c6e597f26767db0e0d
Michele Ferri <ferrimiche@gmail.com>
2020-09-14 21:41:15 +0200

    wip remote spreadsheet function

---
+++ js/spreadsheet.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const fs = require('fs');
+const readline = require('readline');
+const { google } = require('googleapis');
+const ElectronGoogleOAuth2 = require('@getstation/electron-google-oauth2');
+const configuration = require('./configuration');
+configuration.init();
+
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+
+// Load the app credentials file.
+const loadCredentials = () => {
+	if (!fs.existsSync(tnpath)) {
+		return console.log('Error loading application credentials.');
+	}
+	return JSON.parse(fs.readFileSync('credentials.json'));
+};
+
+// Use stored access token and create an API client.
+const getClient = () => {
+	let token = configuration.get('google_spreadsheets_token');
+	if (token == null) {
+		console.log('No access token present. Please use getNewToken function.');
+	}
+
+	let credentials = loadCredentials();
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+	oAuth2Client.setCredentials(JSON.parse(token));
+	return oAuth2Client;
+}
+
+// Start the Oauth2 process to create a new user access token.
+const getNewToken = () => {
+	let credentials = loadCredentials();
+	const {client_secret, client_id, redirect_uris} = credentials.installed;
+	// Authorize a client with credentials, then call the Google Sheets API.
+	const myApiOauth = new ElectronGoogleOAuth2(client_id, client_secret, redirect_uris[0]);
+	myApiOauth.openAuthWindowAndGetTokens()
+		.then(token => {
+			// Store the token to disk for later program executions.
+			configuration.set('google_spreadsheets_token', JSON.stringify(token));
+		});
+};
+
+const create = (title) => {
+	const client = getClient();
+	client.sheetsService.spreadsheets.create({
+		properties: {
+			title,
+		},
+		fields: 'spreadsheetId',
+	}, (err, spreadsheet) =>{
+		if (err) {
+			// Handle error.
+			console.log(err);
+		}
+		else {
+			return spreadsheet.spreadsheetId;
+		}
+	});
+};
+
+const write = () => {
+	let values = [
+		[
+			// Cell values ...
+		],
+		// Additional rows ...
+	];
+	const resource = {
+		values,
+	};
+	const client = getClient();
+	client.sheetsService.spreadsheets.values.update({
+		spreadsheetId,
+		range,
+		valueInputOption,
+		resource,
+	}, (err, result) => {
+		if (err) {
+			// Handle error
+			console.log(err);
+		} else {
+			console.log('%d cells updated.', result.updatedCells);
+		}
+	});
+};
