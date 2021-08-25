0e62a2911db702752a50caba1509c9a089fbbc28
Pimentoso <zomgsako@gmail.com>
2021-08-25 19:12:10 +0200

    remove stdin hack

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
 'use strict';
 
-// Johnny-Five uses stdin, which causes Electron to crash
-// this reroutes stdin, so it can be used
-const Readable = require('stream').Readable;
-const util = require('util');
-util.inherits(MyStream, Readable);
-function MyStream(opt) {
-	Readable.call(this, opt);
-}
-MyStream.prototype._read = function () { };
-// hook in our stream
-process.__defineGetter__("stdin", function () {
-	if (process.__stdin) return process.__stdin;
-	process.__stdin = new MyStream();
-	return process.__stdin;
-});
-
 ////////////////////////
 const debugMode = false;
 ////////////////////////
